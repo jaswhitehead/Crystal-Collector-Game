36a2bde4804bfc23e5f504b9d475a82517ecd682
Jason Whitehead <jaswhitehead@gmail.com>
2020-04-29 18:22:45 -0500

    Started javascripting and pushed game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// GLOBAL VARIABLES
+
+// Crystal Variables
+var crystal = {
+    red:
+    {
+        name: "Red",
+        value: 0
+    },
+    white:
+    {
+        name: "White",
+        value: 0
+    },
+    yellow:
+    {
+        name: "Yellow",
+        value: 0
+    },
+    purple:
+    {
+        name: "Purple",
+        value: 0
+    },
+    blue:
+    {
+        name: "Blue",
+        value: 0
+    },
+}
